2b956d16ef9aef1c4884f0e0e47870e823740b8b
marjuly <122mcruz@frhsd.com>
2022-03-28 12:54:21 -0400

    getMonth 1

--- site.js
+++ site.js
@@ -41,52 +41,53 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-                // getMonthText(dateArray) {
-                //   months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                //   releaseDate
-                //   switch(dateArray[2]){
-                //     case 1:
-                //       releaseDate =
-                //       break
-                //     case 2:
-                //       month = "February"
-                //       break
-                //     case 3:
-                //       month = "March"
-                //       break
-                //     case 4:
-                //       month = "April"
-                //       break
-                //     case 5:
-                //       month = "May"
-                //       break
-                //     case 6:
-                //       month = "June"
-                //       break
-                //     case 7:
-                //       month = "July"
-                //       break
-                //     case 8:
-                //       month = "August"
-                //       break
-                //     case 9:
-                //       month = "September"
-                //       break
-                //     case 10:
-                //       month = "October"
-                //       break
-                //     case 11:
-                //       month = "November"
-                //       break
-                //     case 12:
-                //       month = "December"
-                //       break
-                //   }
-                //   console.log(month + movies[index].released[3] + ", " + movies[index].released[1])
-                // },
-                // runGetMonth(){
-                //   this.getMonthText(movies[index].released)
-                // }
+                getMonthText(dateArray) {
+                  months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                  releaseDate
+                  switch(dateArray[1]){
+                    case 1:
+                      month = "January"
+                      break
+                    case 2:
+                      month = "February"
+                      break
+                    case 3:
+                      month = "March"
+                      break
+                    case 4:
+                      month = "April"
+                      break
+                    case 5:
+                      month = "May"
+                      break
+                    case 6:
+                      month = "June"
+                      break
+                    case 7:
+                      month = "July"
+                      break
+                    case 8:
+                      month = "August"
+                      break
+                    case 9:
+                      month = "September"
+                      break
+                    case 10:
+                      month = "October"
+                      break
+                    case 11:
+                      month = "November"
+                      break
+                    case 12:
+                      month = "December"
+                      break
+                  }
+                  releaseDate = month + dateArray[2] + ", " + dateArray[0]
+                  return releaseDate
+                },
+                runGetMonth(){
+                  this.getMonthText(movies[index].released)
+                }
                 posterClick(index){
                   index++
                   if (index >= movies[index].posters) {
