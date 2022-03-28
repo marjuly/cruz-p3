fc06b1e7853f5f4bdf22c145f56d9c4cd907e3fc
marjuly <122mcruz@frhsd.com>
2022-03-28 12:07:52 -0400

    method test

--- site.js
+++ site.js
@@ -42,47 +42,17 @@ const vue_app = Vue.createApp({
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
             getMonthText(dateArray) {
-              switch(dateArray){
-                case 1:
-                  month = "January";
-                  break;
-                case 2:
-                  month = "February";
-                  break;
-                case 3:
-                  month = "March";
-                  break;
-                case 4:
-                  month = "April";
-                  break;
-                case 5:
-                  month = "May";
-                  break;
-                case 6:
-                  month = "June";
-                  break;
-                case 7:
-                  month = "July";
-                  break;
-                case 8:
-                  month = "August";
-                  break;
-                case 9:
-                  month = "September";
-                  break;
-                case 10:
-                  month = "October";
-                  break;
-                case 11:
-                  month = "November";
-                  break;
-                case 12:
-                  month = "December";
-                  break;
+              months = ["January", "February", "March", "April","May", "June", "July", "August", "September", "October", "November", "December"];
+              switch(dateArray = ){
+                
               }
+            },
+            loadTest(sample) {
+              alert(sample)
+            },
+            beforeMount(){
+              this.loadTest(12)
             }
-
-
       }
 })
 
