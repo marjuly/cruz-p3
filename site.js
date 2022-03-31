95b1d5d94951c09b5d76ca9e1a8d1ed87fec6d98
marjuly <122mcruz@frhsd.com>
2022-03-31 12:06:13 -0400

    hide/show fixed

--- site.js
+++ site.js
@@ -37,50 +37,51 @@ const vue_app = Vue.createApp({
             title: "IMDB + Marjuly's Top 8 Movies",
             owner: "Marjuly",
             github: "https://github.com/marjuly/cruz-p3",
+            showRelease: true
+            releaseDate: "This is the date the movie was released";
       }
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
                 getMonthText(dateArray) {
                   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                  var releaseDate;
                   var month;
                   switch(dateArray[1]){
                     case 1:
-                      month = months[0] + " "
+                      month = months[0]
                       break
                     case 2:
-                      month = months[1] + " "
+                      month = months[1]
                       break
                     case 3:
-                      month = months[2] + " "
+                      month = months[2]
                       break
                     case 4:
-                      month = months[3] + " "
+                      month = months[3]
                       break
                     case 5:
-                      month = months[4] + " "
+                      month = months[4]
                       break
                     case 6:
-                      month = months[5] + " "
+                      month = months[5]
                       break
                     case 7:
-                      month = months[6] + " "
+                      month = months[6]
                       break
                     case 8:
-                      month = months[7] + " "
+                      month = months[7]
                       break
                     case 9:
-                      month = months[8] + " "
+                      month = months[8]
                       break
                     case 10:
-                      month = months[9] + " "
+                      month = months[9]
                       break
                     case 11:
-                      month = months[10] + " "
+                      month = months[10]
                       break
                     case 12:
-                      month = months[11] + " "
+                      month = months[11]
                       break
                   }
                   releaseDate = month + " " + dateArray[2] + ", " + dateArray[0]
